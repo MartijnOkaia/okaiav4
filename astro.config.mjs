9d93203eae06a3d70480b1f0ed2f5e9b46fa3ef6
MartijnOkaia <martijn@okaia.nl>
2023-09-12 22:53:28 +0200

    Added Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,27 @@ import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import netlify from "@astrojs/netlify/functions";
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: netlify(),
-    integrations: [
-        storyblok({
-        accessToken: env.STORYBLOK_TOKEN,
-        bridge: true,
-        components: {
-            blogPost: 'storyblok/BlogPost',
-            blogPostList: 'storyblok/BlogPostList',
-            page: 'storyblok/Page',
-        }
-        })
-    ],
-    vite: {
-        plugins: [basicSsl()],
-        server: {
-            https: true,
-        },
-    },
+  output: "server",
+  adapter: netlify(),
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    bridge: true,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page'
+    }
+  }), tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  }
 });
