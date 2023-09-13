ba4fbaa2b425a506301569c1ccc764b18d8867f9
MartijnOkaia <martijn@okaia.nl>
2023-09-13 12:42:37 +0200

    Draft or published?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import netlify from "@astrojs/netlify/functions";
+
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import storyblok from '@storyblok/astro';
+
+import netlify from "@astrojs/netlify/functions";
+
 import tailwind from "@astrojs/tailwind";
+
+import { loadEnv } from 'vite';
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
